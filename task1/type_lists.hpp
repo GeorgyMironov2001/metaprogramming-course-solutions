d016df09e60d17c644dfb69aff0652c52f271075
GeorgyMironov2001 <61285161+GeorgyMironov2001@users.noreply.github.com>
2022-12-24 21:08:20 +0300

    Update type_lists.hpp

--- task1/type_lists.hpp
+++ task1/type_lists.hpp
@@ -1,122 +1,106 @@
 #pragma once
 
 #include <concepts>
-#include <stddef.h>
+#include <cstddef>
 #include "type_tuples.hpp"
-using type_tuples::TTuple;
-namespace type_lists
-{
-    
 
-    template <class TL>
+using type_tuples::TTuple;
+namespace type_lists {
+
+
+    template<class TL>
     concept TypeSequence = requires
     {
         typename TL::Head;
         typename TL::Tail;
     };
 
-    struct Nil
-    {
+    struct Nil {
     };
 
-    template <class TL>
+    template<class TL>
     concept Empty = std::derived_from<TL, Nil>;
 
-    template <class TL>
+    template<class TL>
     concept TypeList = Empty<TL> || TypeSequence<TL>;
 
     // Your fun, fun metaalgorithms :)
-    template <class T, class TL>
-    struct Cons
-    {
+    template<class T, class TL>
+    struct Cons {
         using Head = T;
         using Tail = TL;
     };
 
-    template <class T>
-    struct Repeat
-    {
+    template<class T>
+    struct Repeat {
         using Head = T;
         using Tail = Repeat<T>;
     };
 
-    template <typename... TL>
-    struct Help_struct_for_to_tuple
-    {
+    template<typename... TL>
+    struct Help_struct_for_to_tuple {
     };
 
-    template <class T, class Tail>
-    struct Help_struct_for_to_tuple<Cons<T, Tail>>
-    {
+    template<class T, class Tail>
+    struct Help_struct_for_to_tuple<Cons<T, Tail>> {
         using Type = typename Help_struct_for_to_tuple<Tail, T>::Type;
     };
 
-    template <class T, class Tail, class... U>
-    struct Help_struct_for_to_tuple<Cons<T, Tail>, U...>
-    {
+    template<class T, class Tail, class... U>
+    struct Help_struct_for_to_tuple<Cons<T, Tail>, U...> {
         using Type = typename Help_struct_for_to_tuple<Tail, U..., T>::Type;
     };
-    template <class TL>
-    requires(TypeSequence<TL>) struct Help_struct_for_to_tuple<TL>
-    {
+    template<class TL> requires(TypeSequence<TL>)
+    struct Help_struct_for_to_tuple<TL> {
         using Type = typename Help_struct_for_to_tuple<typename TL::Tail, typename TL::Head>::Type;
     };
-    template <class TL, class... U>
-    requires(TypeSequence<TL>) struct Help_struct_for_to_tuple<TL, U...>
-    {
+    template<class TL, class... U> requires(TypeSequence<TL>)
+    struct Help_struct_for_to_tuple<TL, U...> {
         using Type = typename Help_struct_for_to_tuple<typename TL::Tail, U..., typename TL::Head>::Type;
     };
-    template <class... U>
-    struct Help_struct_for_to_tuple<Nil, U...>
-    {
+    template<class... U>
+    struct Help_struct_for_to_tuple<Nil, U...> {
         using Type = TTuple<U...>;
     };
 
-    template <typename T>
+    template<typename T>
     using ToTuple = typename Help_struct_for_to_tuple<T>::Type;
 
-    template <class Tuple>
-    struct Help_struct_for_from_tuple
-    {
+    template<class Tuple>
+    struct Help_struct_for_from_tuple {
     };
 
-    template <typename T, typename... Tail>
-    struct Help_struct_for_from_tuple<TTuple<T, Tail...>>
-    {
+    template<typename T, typename... Tail>
+    struct Help_struct_for_from_tuple<TTuple<T, Tail...>> {
         using Type = Cons<T, typename Help_struct_for_from_tuple<TTuple<Tail...>>::Type>;
     };
 
-    template <typename T>
-    struct Help_struct_for_from_tuple<TTuple<T>>
-    {
+    template<typename T>
+    struct Help_struct_for_from_tuple<TTuple<T>> {
         using Type = Cons<T, Nil>;
     };
 
-    template <class Tuple>
+    template<class Tuple>
     using FromTuple = typename Help_struct_for_from_tuple<Tuple>::Type;
 
-    template <size_t N, typename T>
-    struct Help_struct_for_take
-    {
+    template<size_t N, typename T>
+    struct Help_struct_for_take {
         using Type = Cons<typename T::Head, typename Help_struct_for_take<N - 1, typename T::Tail>::Type>;
     };
-    template <typename T>
-    struct Help_struct_for_take<0, T>
-    {
+    template<typename T>
+    struct Help_struct_for_take<0, T> {
         using Type = Nil;
     };
-    template <size_t N>
-    struct Help_struct_for_take<N, Nil>
-    {
+    template<size_t N>
+    struct Help_struct_for_take<N, Nil> {
         using Type = Nil;
     };
 
-    template <>
-    struct Help_struct_for_take<0, Nil>
-    {
+    template<>
+    struct Help_struct_for_take<0, Nil> {
         using Type = Nil;
     };
-    template <size_t N, typename T>
+    template<size_t N, typename T>
     using Take = typename Help_struct_for_take<N, T>::Type;
 
     template<int N, typename T>
@@ -137,50 +121,43 @@ namespace type_lists
         using Type = Nil;
     };
 
-    template <int N, typename T>
+    template<int N, typename T>
     using Drop = typename Drop_struct<N, T>::Type;
 
-    template <size_t N, typename T>
-    struct Replicate_
-    {
+    template<size_t N, typename T>
+    struct Replicate_ {
         using Type = Cons<T, typename Replicate_<N - 1, T>::Type>;
     };
-    template <typename T>
-    struct Replicate_<1, T>
-    {
+    template<typename T>
+    struct Replicate_<1, T> {
         using Type = Cons<T, Nil>;
     };
 
-    template <size_t N, typename T>
+    template<size_t N, typename T>
     using Replicate = typename Replicate_<N, T>::Type;
 
-    template <template <class> typename F, typename T>
-    struct Iterate
-    {
+    template<template<class> typename F, typename T>
+    struct Iterate {
         using Head = T;
         using Tail = Iterate<F, F<T>>;
     };
 
-    template <typename... TL>
-    struct Cycle
-    {
+    template<typename... TL>
+    struct Cycle {
     };
 
-    template <typename TL>
-    struct Cycle<TL>
-    {
+    template<typename TL>
+    struct Cycle<TL> {
         using Head = typename TL::Head;
         using Tail = Cycle<TL, typename TL::Tail>;
     };
-    template <typename TL, typename T>
-    struct Cycle<TL, T>
-    {
+    template<typename TL, typename T>
+    struct Cycle<TL, T> {
         using Head = typename T::Head;
         using Tail = Cycle<TL, typename T::Tail>;
     };
-    template <typename TL>
-    struct Cycle<TL, Nil>
-    {
+    template<typename TL>
+    struct Cycle<TL, Nil> {
         using Head = typename TL::Head;
         using Tail = Cycle<TL, typename TL::Tail>;
     };
@@ -196,35 +173,33 @@ namespace type_lists
     template<template<class> typename F, typename TL>
     using Map_struct = typename Map_struct<F, TL>::Type;*/
 
-    template <template <class> typename F, typename TL>
-    struct Map_struct
-    {
+    template<template<class> typename F, typename TL>
+    struct Map_struct {
         using Head = F<typename TL::Head>;
         using Tail = Map_struct<F, typename TL::Tail>;
     };
-    template <template <class> typename F, typename TL>
-    requires(std::same_as<typename TL::Tail, Nil>) struct Map_struct<F, TL>
-    {
+    template<template<class> typename F, typename TL> requires(std::same_as<typename TL::Tail, Nil>)
+    struct Map_struct<F, TL> {
         using Head = F<typename TL::Head>;
         using Tail = Nil;
     };
-    template <template <class> typename F, typename TL>
-    struct Map_Nil
-    {
+    template<template<class> typename F, typename TL>
+    struct Map_Nil {
         using Type = Map_struct<F, TL>;
     };
-    template <template <class> typename F, typename TL>
-    requires(std::same_as<TL, Nil>) struct Map_Nil<F, TL>
-    {
+    template<template<class> typename F, typename TL> requires(std::same_as<TL, Nil>)
+    struct Map_Nil<F, TL> {
         using Type = Nil;
     };
-    template <template <class> typename F, typename TL>
+    template<template<class> typename F, typename TL>
     using Map = typename Map_Nil<F, TL>::Type;
-    template <bool, typename TL, template <class> typename P>
-    struct struct_for_filter1
-    {
+
+    template<bool, typename TL, template<class> typename P>
+    struct struct_for_filter1 {
     };
 
+
+    /*
     template <bool b, template <class> typename P>
     struct struct_for_filter1<b, Nil, P>
     {
@@ -262,176 +237,191 @@ namespace type_lists
         using Type = Nil;
     };
     template <template <class> typename P, typename TL>
-    using Filter = typename struct_for_filter2<P, TL>::Type;
+    using Filter = typename struct_for_filter2<P, TL>::Type;*/
 
-    template <template <class, class> typename OP, typename T, typename TL>
-    struct scan1
-    {
+    template<bool b, template<class> typename P, typename TL>
+    struct next_fit_for_P {
+
+    };
+    template<template<class> typename P, typename TL>
+    struct next_fit_for_P<true, P, TL> {
+        using T = TL;
+    };
+    template<template<class> typename P>
+    struct next_fit_for_P<true, P, Nil> {
+        using T = Nil;
+    };
+    template<template<class> typename P, typename TL>
+    struct next_fit_for_P<false, P, TL> {
+        using T = typename next_fit_for_P<P<typename TL::Tail::Head>::Value, P, typename TL::Tail>::T;
+    };
+    template<template<class> typename P>
+    struct next_fit_for_P<false, P, Nil> {
+        using T = Nil;
+    };
+    template<template<class> typename P, typename TL>
+    struct Filter1;
+
+    template<template<class> typename P, typename TL>
+    struct help_for_filter {
+        using Type = Filter1<P, TL>;
+    };
+    template<template<class> typename P>
+    struct help_for_filter<P, Nil> {
+        using Type = Nil;
+    };
+    template<template<class> typename P, typename TL>
+    struct Filter1 {
+        using block = typename next_fit_for_P<P<typename TL::Head>::Value, P, TL>::T;
+        using Head = typename block::Head;
+        using Tail = typename help_for_filter<P, typename block::Tail>::Type;
+    };
+    template<template<class> typename P, typename TL>
+    using Filter = typename help_for_filter<P, TL>::Type;
+
+
+    template<template<class, class> typename OP, typename T, typename TL>
+    struct scan1 {
         using Type = Cons<OP<T, typename TL::Head>, typename scan1<OP, OP<T, typename TL::Head>, typename TL::Tail>::Type>;
     };
-    template <template <class, class> typename OP, typename T>
-    struct scan1<OP, T, Nil>
-    {
+    template<template<class, class> typename OP, typename T>
+    struct scan1<OP, T, Nil> {
         using Type = Nil;
     };
 
-    template <template <class, class> typename OP, typename T, typename TL>
+    template<template<class, class> typename OP, typename T, typename TL>
     using Scanl = Cons<T, typename scan1<OP, T, TL>::Type>;
 
-    template <template <class, class> typename OP, typename T, typename TL>
-    struct fold1
-    {
+    template<template<class, class> typename OP, typename T, typename TL>
+    struct fold1 {
         using Type = typename fold1<OP, OP<T, typename TL::Head>, typename TL::Tail>::Type;
     };
-    template <template <class, class> typename OP, typename T>
-    struct fold1<OP, T, Nil>
-    {
+    template<template<class, class> typename OP, typename T>
+    struct fold1<OP, T, Nil> {
         using Type = T;
     };
-    template <template <class, class> typename OP, typename T, typename TL>
+    template<template<class, class> typename OP, typename T, typename TL>
     using Foldl = typename fold1<OP, T, TL>::Type;
 
-    template <typename TL, size_t N, typename T>
-    struct inits_struct
-    {
+    template<typename TL, size_t N, typename T>
+    struct inits_struct {
         using Type = Cons<Take<N, TL>, typename inits_struct<TL, N + 1, typename T::Tail>::Type>;
     };
-    template <typename TL, size_t N>
-    struct inits_struct<TL, N, Nil>
-    {
+    template<typename TL, size_t N>
+    struct inits_struct<TL, N, Nil> {
         using Type = Nil;
     };
-    template <typename TL>
+    template<typename TL>
     using Inits = Cons<Nil, typename inits_struct<TL, 1, TL>::Type>;
 
-    template <typename TL, size_t N, typename T>
-    struct tails_struct
-    {
+    template<typename TL, size_t N, typename T>
+    struct tails_struct {
         using Type = Cons<Drop<N, TL>, typename tails_struct<TL, N + 1, typename T::Tail>::Type>;
     };
-    template <typename TL, size_t N>
-    struct tails_struct<TL, N, Nil>
-    {
+    template<typename TL, size_t N>
+    struct tails_struct<TL, N, Nil> {
         using Type = Nil;
     };
-    template <typename TL>
+    template<typename TL>
     using Tails = Cons<TL, typename tails_struct<TL, 1, TL>::Type>;
 
-    template <typename L, typename R>
-    struct Zip2_struct
-    {
+    template<typename L, typename R>
+    struct Zip2_struct {
         using Head = TTuple<typename L::Head, typename R::Head>;
         using Tail = Zip2_struct<typename L::Tail, typename R::Tail>;
     };
-    template <typename L, typename R>
-    requires(std::same_as<typename L::Tail, Nil> ||
-             std::same_as<typename R::Tail, Nil>) struct Zip2_struct<L, R>
-    {
+    template<typename L, typename R> requires (std::same_as<typename L::Tail, Nil> ||
+                                               std::same_as<typename R::Tail, Nil>)
+    struct Zip2_struct<L, R> {
         using Head = TTuple<typename L::Head, typename R::Head>;
         using Tail = Nil;
     };
 
-    template <typename L, typename R>
-    struct base
-    {
+    template<typename L, typename R>
+    struct base {
         using Type = Zip2_struct<L, R>;
     };
 
-    template <typename L, typename R>
-    requires(std::same_as<L, Nil> ||
-             std::same_as<R, Nil>) struct base<L, R>
-    {
+    template<typename L, typename R> requires (std::same_as<L, Nil> ||
+                                               std::same_as<R, Nil>)
+    struct base<L, R> {
         using Type = Nil;
     };
-    template <typename L, typename R>
+    template<typename L, typename R>
     using Zip2 = typename base<L, R>::Type;
 
-    template <typename TL>
-    struct get_head_cons
-    {
+    template<typename TL>
+    struct get_head_cons {
     };
-    template <typename T, typename TL>
-    requires(!std::same_as<T, Nil>) struct get_head_cons<Cons<T, TL>>
-    {
+    template<typename T, typename TL> requires (!std::same_as<T, Nil>)
+    struct get_head_cons<Cons<T, TL>> {
         using Type = Cons<typename T::Head, typename get_head_cons<TL>::Type>;
         static constexpr bool no_Nil = get_head_cons<TL>::no_Nil;
     };
-    template <typename T, typename TL>
-    requires(std::same_as<T, Nil>) struct get_head_cons<Cons<T, TL>>
-    {
+    template<typename T, typename TL> requires(std::same_as<T, Nil>)
+    struct get_head_cons<Cons<T, TL>> {
         using Type = Nil;
         static constexpr bool no_Nil = false;
     };
-    template <typename T>
-    requires(!std::same_as<T, Nil>) struct get_head_cons<Cons<T, Nil>>
-    {
+    template<typename T> requires (!std::same_as<T, Nil>)
+    struct get_head_cons<Cons<T, Nil>> {
         using Type = Cons<typename T::Head, Nil>;
         static constexpr bool no_Nil = true;
     };
-    template <typename T>
-    requires(std::same_as<T, Nil>) struct get_head_cons<Cons<T, Nil>>
-    {
+    template<typename T> requires(std::same_as<T, Nil>)
+    struct get_head_cons<Cons<T, Nil>> {
         using Type = Nil;
         static constexpr bool no_Nil = false;
     };
 
-    template <typename ConsTL>
-    struct get_tail_cons
-    {
+    template<typename ConsTL>
+    struct get_tail_cons {
     };
 
-    template <typename T, typename TL>
-    requires(!std::same_as<typename T::Tail, Nil>) struct get_tail_cons<Cons<T, TL>>
-    {
+    template<typename T, typename TL> requires (!std::same_as<typename T::Tail, Nil>)
+    struct get_tail_cons<Cons<T, TL>> {
         using Type = Cons<typename T::Tail, typename get_tail_cons<TL>::Type>;
         static constexpr bool Cons_tail_no_Nil = get_tail_cons<TL>::Cons_tail_no_Nil;
     };
 
-    template <typename T, typename TL>
-    requires(std::same_as<typename T::Tail, Nil>) struct get_tail_cons<Cons<T, TL>>
-    {
+    template<typename T, typename TL> requires(std::same_as<typename T::Tail, Nil>)
+    struct get_tail_cons<Cons<T, TL>> {
         using Type = Nil;
         static constexpr bool Cons_tail_no_Nil = false;
     };
-    template <typename T>
-    requires(!std::same_as<typename T::Tail, Nil>) struct get_tail_cons<Cons<T, Nil>>
-    {
+    template<typename T> requires (!std::same_as<typename T::Tail, Nil>)
+    struct get_tail_cons<Cons<T, Nil>> {
         using Type = Cons<typename T::Tail, Nil>;
         static constexpr bool Cons_tail_no_Nil = true;
     };
-    template <typename T>
-    requires(std::same_as<typename T::Tail, Nil>) struct get_tail_cons<T>
-    {
+    template<typename T> requires(std::same_as<typename T::Tail, Nil>)
+    struct get_tail_cons<T> {
         using Type = Nil;
         static constexpr bool Cons_tail_no_Nil = false;
     };
 
-    template <typename ConsTL>
-    struct zip_struct
-    {
+    template<typename ConsTL>
+    struct zip_struct {
         using Head = ToTuple<typename get_head_cons<ConsTL>::Type>;
         using Tail = zip_struct<typename get_tail_cons<ConsTL>::Type>;
     };
-    template <typename ConsTL>
-    requires(!get_tail_cons<ConsTL>::Cons_tail_no_Nil) struct zip_struct<ConsTL>
-    {
+    template<typename ConsTL> requires (!get_tail_cons<ConsTL>::Cons_tail_no_Nil)
+    struct zip_struct<ConsTL> {
         using Head = ToTuple<typename get_head_cons<ConsTL>::Type>;
         using Tail = Nil;
     };
 
-    template <typename... TL>
-    struct base_zip
-    {
+    template<typename... TL>
+    struct base_zip {
         using Type = zip_struct<FromTuple<TTuple<TL...>>>;
     };
-    template <typename... TL>
-    requires(!get_head_cons<FromTuple<TTuple<TL...>>>::no_Nil) struct base_zip<TL...>
-    {
+    template<typename... TL> requires (!get_head_cons<FromTuple<TTuple<TL...>>>::no_Nil)
+    struct base_zip<TL...> {
         using Type = Nil;
     };
-    template <typename... TL>
+    template<typename... TL>
     using Zip = typename base_zip<TL...>::Type;
-    
+
     template<bool b, template<class, class> typename EQ, typename T, typename TL>
     struct Equal_Head {
         using Block = Nil;
@@ -462,6 +452,4 @@ namespace type_lists
         using Head = Cons<T, Nil>;
         using Tail = Nil;
     };
-    
 }; // namespace type_lists
-
