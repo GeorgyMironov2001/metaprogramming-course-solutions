baec454f2f546f379e588bf8554a78325356e8fa
GeorgyMironov2001 <61285161+GeorgyMironov2001@users.noreply.github.com>
2022-12-26 02:50:37 +0300

    Update Spy.hpp
    Занес сравнение внутрь класса

--- task3/Spy.hpp
+++ task3/Spy.hpp
@@ -7,45 +7,46 @@
 // Allocator is only used in bonus SBO tests,
 // ignore if you don't need bonus points.
 
-struct Base_Logger
-{
+struct Base_Logger {
     virtual ~Base_Logger() = default;
+
+    virtual std::shared_ptr<Base_Logger> create() = 0;
 
     virtual void Add_Counter(int n) = 0;
 };
 
-template <class Logger>
-struct Derived_Logger : Base_Logger
-{
+template<class Logger>
+struct Derived_Logger : Base_Logger {
     Logger logger_;
 
-    void Add_Counter(int n) override
-    {
+    void Add_Counter(int n) override {
         logger_(n);
+    }
+
+    std::shared_ptr<Base_Logger> create() override {
+
+        auto d = std::shared_ptr<Base_Logger>(new Derived_Logger<Logger>(std::forward<Logger>(logger_)));
+        return d;
     }
 
     explicit Derived_Logger(Logger &&logger) : logger_(std::forward<Logger>(logger)) {}
 };
 
-template <class T /*, class Allocator = std::allocator<std::byte>*/>
+template<class T /*, class Allocator = std::allocator<std::byte>*/>
 class Spy;
 
-template <typename T>
-struct Helper
-{
+template<typename T>
+struct Helper {
     explicit Helper(Spy<T> &s) : counter_(s.counter_), value_(&s.value_), logger_ptr_(s.logger_ptr_) {}
 
     explicit Helper(Spy<T> &&s) : counter_(s.counter_), value_(&s.value_), logger_ptr_(std::move(s.logger_ptr_)) {}
 
-    T *operator->()
-    {
+    T *operator->() {
         return value_;
     }
 
-    ~Helper()
-    {
-        if (*counter_ != 0 && logger_ptr_ != nullptr)
-        {
+    ~Helper() {
+        if (*counter_ != 0 && logger_ptr_ != nullptr) {
             // std::cout << *counter_ << "\n";
             logger_ptr_->Add_Counter(*counter_);
         }
@@ -58,9 +59,8 @@ struct Helper
     std::shared_ptr<Base_Logger> logger_ptr_{nullptr};
 };
 
-template <class T /*, class Allocator = std::allocator<std::byte>*/>
-class Spy
-{
+template<class T /*, class Allocator = std::allocator<std::byte>*/>
+class Spy {
 public:
     Spy() = default;
 
@@ -68,50 +68,44 @@ public:
 
     Spy(T &&value) : value_(std::move(value)) {}
 
-    T &operator*()
-    {
+    T &operator*() {
         return value_;
     }
 
-    const T &operator*() const
-    {
+    const T &operator*() const {
         return value_;
     }
 
-    auto operator->()
-    {
+    auto operator->() {
         *counter_ += 1;
         // return this->shared_from_this();
 
         return (Helper<T>(*this));
     }
 
-    Spy(const Spy<T> &other) requires(std::copyable<T>) : value_(other.value_),
-                                                          logger_ptr_(other.logger_ptr_)
-    {
+    Spy(const Spy<T> &other) requires(std::copyable<T>): value_(other.value_) {
+
+        logger_ptr_ = other.logger_ptr_ == nullptr ? nullptr : other.logger_ptr_->create();
         counter_ = std::make_shared<int>(0);
     }
 
-    Spy(Spy<T> &&other) noexcept requires(std::movable<T>) : value_(std::move(other.value_)),
-                                                             logger_ptr_(std::move(other.logger_ptr_))
-    {
+    Spy(Spy<T> &&other) noexcept requires(std::movable<T>): value_(std::move(other.value_)) {
+        logger_ptr_ = other.logger_ptr_ == nullptr ? nullptr : other.logger_ptr_->create();
         counter_ = std::make_shared<int>(0);
     }
 
-    Spy &operator=(const Spy<T> &other)
-    {
+    Spy &operator=(const Spy<T> &other) {
         value_ = other.value_;
         counter_ = std::make_shared<int>(0);
-        logger_ptr_ = other.logger_ptr_;
+        logger_ptr_ = other.logger_ptr_ == nullptr ? nullptr : other.logger_ptr_->create();
 
         return *this;
     }
 
-    Spy &operator=(Spy<T> &&other) noexcept
-    {
+    Spy &operator=(Spy<T> &&other) noexcept {
         value_ = std::move(other.value_);
         counter_ = std::make_shared<int>(0);
-        logger_ptr_ = other.logger_ptr_;
+        logger_ptr_ = other.logger_ptr_ == nullptr ? nullptr : other.logger_ptr_->create();
 
         return *this;
     }
@@ -125,17 +119,26 @@ public:
      */
 
     // Resets logger
-    void setLogger();
+    void setLogger() {
+        logger_ptr_ = nullptr;
+    }
 
-    template <class Logger>
+    template<class Logger>
     /*requires  see task readme */
-    void setLogger(Logger &&logger) requires((!std::copyable<T> || std::copyable<std::remove_reference_t<Logger>>)&&(!std::movable<T> || std::movable<std::remove_reference_t<Logger>>))
-    {
+    void setLogger(Logger &&logger) requires ((!std::copyable<T> || std::copyable<std::remove_reference_t<Logger>>) &&
+                                              (!std::movable<T> || std::movable<std::remove_reference_t<Logger>>)) {
         logger_ptr_ = std::shared_ptr<Base_Logger>(new Derived_Logger<Logger>(std::forward<Logger>(logger)));
     }
 
     ~Spy() {
-        
+
+    }
+
+public:
+    template<class U>
+    friend bool
+    operator==(const Spy<T> &l, const Spy<U> &r) requires (std::same_as<T, U> && std::equality_comparable<T>) {
+        return l.value_ == r.value_;
     }
 
 public:
@@ -145,8 +148,3 @@ public:
     std::shared_ptr<Base_Logger> logger_ptr_{nullptr};
 };
 
-template <class T, class U>
-bool operator==(const Spy<T> &l, const Spy<U> &r) requires(std::same_as<T, U> &&std::equality_comparable<T>)
-{
-    return l.value_ == r.value_;
-}
